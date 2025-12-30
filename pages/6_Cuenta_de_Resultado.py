97192b3d963ebb7e93ae48e0a5613918dabf00b5
juanjoserivas2013-dot <juanjoserivas2013@gmail.com>
2025-12-30 14:32:50 +0100

    Update 6_Cuenta_de_Resultado.py

--- pages/6_Cuenta_de_Resultado.py
+++ pages/6_Cuenta_de_Resultado.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 
-st.set_page_config(page_title="Cuenta de Resultados", layout="centered")
 st.title("Cuenta de Resultados")
 
 DATA_PATH = Path("data")
