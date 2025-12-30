fa3113808ea00e60becc1fa32ff1978009d6a763
juanjoserivas2013-dot <juanjoserivas2013@gmail.com>
2025-12-30 23:30:47 +0100

    Update 10_Gastos.py

--- pages/10_Gastos.py
+++ pages/10_Gastos.py
@@ -165,13 +165,14 @@ else:
 st.caption(f"Año base para Cuenta de Resultados: {anio_base}")
 
 # Crear estructura enero–diciembre
-meses = [
-    "01", "02", "03", "04", "05", "06",
-    "07", "08", "09", "10", "11", "12"
+
+MESES_TEXTO = [
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
 ]
 
 base_meses = pd.DataFrame({
-    "Mes": [f"{anio_base}-{m}" for m in meses]
+    "Mes": MESES_TEXTO
 })
 
 # Agrupar gastos reales
