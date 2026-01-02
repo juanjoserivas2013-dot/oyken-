1890c9c4c19a51598069ad49dc4347cecc20010d
juanjoserivas2013-dot <juanjoserivas2013@gmail.com>
2026-01-02 02:46:15 +0100

    Update 2_Totalidades.py

--- pages/2_Totalidades.py
+++ pages/2_Totalidades.py
@@ -18,11 +18,24 @@ st.subheader("Contexto temporal")
 
 anio_actual = date.today().year
 
-anio_sel = st.selectbox(
-    "Año",
-    options=[anio_actual],
-    index=0
-)
+col1, col2 = st.columns(2)
+
+with col1:
+    anio_sel = st.selectbox(
+        "Año",
+        options=[anio_actual],
+        index=0
+    )
+
+with col2:
+    mes_sel = st.selectbox(
+        "Mes",
+        options=[
+            "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+            "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        ],
+        index=date.today().month - 1
+    )
 
 # =========================
 # CARGA DEL CSV
